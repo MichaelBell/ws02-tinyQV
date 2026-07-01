27ee2e2016a9e2f07ed7415342b3e3f8c8aec6be
Mike Bell <mdb036@gmail.com>
2026-07-01 22:10:25 +0100

    Fix various things

--- src/video/text_ram.v
+++ src/video/text_ram.v
@@ -12,7 +12,7 @@ module text_ram #(parameter ADDR_BITS=12) (
 );
 
     wire [2:0] write_enable_n;
-    wire [2:0] ram_select_n;
+    reg [1:0] ram_select_r;
     wire [7:0] data_out0;
     wire [7:0] data_out1;
     wire [7:0] data_out2;
@@ -49,17 +49,16 @@ module text_ram #(parameter ADDR_BITS=12) (
 
     generate
         for (genvar i=0; i<3; i += 1) begin : select
-            assign ram_select_n[i] = i == data_addr[11:10];
             assign write_enable_n[i] = data_write_n || (i != data_addr[11:10]);
         end
     endgenerate
 
-    //always @(posedge clk) begin
-    //    data_out <= ram_select_n[0] ? data_out0 :
-    //                ram_select_n[1] ? data_out1 : data_out2;
-    //end
+    always @(posedge clk) begin
+        ram_select_r[0] <= data_addr[11:10] == 0;
+        ram_select_r[1] <= data_addr[10];
+    end
 
-    assign data_out = ram_select_n[0] ? data_out0 :
-                      ram_select_n[1] ? data_out1 : data_out2;
+    assign data_out = ram_select_r[0] ? data_out0 :
+                      ram_select_r[1] ? data_out1 : data_out2;
 
 endmodule
